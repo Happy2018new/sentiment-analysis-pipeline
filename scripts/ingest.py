b7b575ba124f27c22e7b9385dbfa2aa77e37f5c8
Happy2018new <chenbo79800@163.com>
2025-12-11 21:08:12 +0800

    scripts/ingest.py: Provide ingest reader
    Well, I think this implemention is horrible because current implemention is not fit to the actual environment.
    
    The real life is some APIs like the client make some HTTP requests to send messages to the server (Maybe the client is the comments server).
    
    So it's impossible here to just have a single file...

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -1,1 +1,28 @@
-# Placeholder for ingest.py
+import json
+
+
+class IngestReader:
+    """IngestReader is the reader that read the ingested data from a file."""
+
+    file_path: str
+
+    def __init__(self, file_path: str):
+        """Creates and returns a new reader
+
+        Args:
+            file_path (str): The path point to the ingested data file.
+        """
+        self.file_path = file_path
+
+    def read(self) -> list[dict]:
+        """read reads all the ingested data from the file.
+
+        Returns:
+            list[dict]: All the ingested data from the file.
+                        Each element in this list is a basic JSON dict.
+        """
+        streams: list[dict] = []
+        with open(self.file_path, "r+", encoding="utf-8") as file:
+            for line in file:
+                streams.append(json.loads(line))
+        return streams
