36130cd0b3ea6f62d42874286229244e9b354974
Happy2018new <chenbo79800@163.com>
2025-12-11 21:30:13 +0800

    scripts/ingest.py: Minor enhance

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -1,28 +1,35 @@
 import json
+from typing import Any
 
 
 class IngestReader:
     """IngestReader is the reader that read the ingested data from a file."""
 
-    file_path: str
+    comments: list[dict[str, Any]]
 
     def __init__(self, file_path: str):
-        """Creates and returns a new reader
+        """
+        Creates and returns a new reader.
+
+        Ensure that comments will have the
+        payload corresponding to the ingested
+        data after this method is called.
 
         Args:
             file_path (str): The path point to the ingested data file.
         """
-        self.file_path = file_path
+        self._read(file_path)
 
-    def read(self) -> list[dict]:
-        """read reads all the ingested data from the file.
-
-        Returns:
-            list[dict]: All the ingested data from the file.
-                        Each element in this list is a basic JSON dict.
+    def _read(self, file_path: str) -> None:
         """
-        streams: list[dict] = []
-        with open(self.file_path, "r+", encoding="utf-8") as file:
+        _read reads all the ingested data from the file,
+        and store the read data into the `self.comments`.
+
+        Args:
+            file_path (str): The path point to the ingested data file.
+        """
+        streams: list[dict[str, Any]] = []
+        with open(file_path, "r+", encoding="utf-8") as file:
             for line in file:
                 streams.append(json.loads(line))
-        return streams
+        self.comments = streams
